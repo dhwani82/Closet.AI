ce8fece670f6358607e17a7cf55664ebc84ad1f0
dhwani <dchauha@gmu.edu>
2026-07-10 13:58:06 -0400

    Add gitignore and initial commit

---
+++ Iosapp/IosappApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosappApp.swift
+//  Iosapp
+//
+//  Created by Aahan Jain on 7/9/26.
+//
+
+import SwiftUI
+
+@main
+struct IosappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
