6fd943001582794fccf7e816ad6725e861d2212c
aahan14jain <aahan.14jain@gmail.com>
2026-07-11 16:24:51 -0700

    Initial Commit

---
+++ ClosetAI/ClosetAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClosetAIApp.swift
+//  ClosetAI
+//
+//  Created by Aahan Jain on 7/11/26.
+//
+
+import SwiftUI
+
+@main
+struct ClosetAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
